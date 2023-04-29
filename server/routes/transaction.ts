33a298175404c154da36be37a7ccb9aeb881c258
Andre Thomas <drethedevjs@gmail.com>
2023-04-28 22:49:46 -0400

    Added transaction type and built out GET and POST routes.

--- server/routes/transaction.ts
+++ server/routes/transaction.ts
@@ -1,11 +1,29 @@
 import express from 'express';
+import Chance from 'chance';
+import { Transaction } from '../types/Transaction.js';
+
 const router = express.Router();
+const random = new Chance();
+let transactions: Transaction[] = [];
 
 router.get("/", (req, res) => {
-  res.send("Getting transactions.");
+  if (!transactions.length) {
+    transactions = createTransactions();
+  }
+
+  res.json(transactions);
 });
 
 router.post("/", (req, res) => {
+  let transaction: Transaction = {
+    id: transactions.length + 1, // Does not mimic db identity column because you can delete say id 5 but the next created one should be 6.
+    name: req.body.name,
+    amount: req.body.amount,
+    date: new Date(),
+    donation: req.body.donation
+  };
+
+  transactions.push(transaction)
   res.send("Transaction added.");
 });
 
@@ -16,6 +34,21 @@ router.put("/", (req, res) => {
 router.delete("/", (req, res) => {
   res.send("Transaction deleted.");
 });
+
+function createTransactions(): Transaction[] {
+  let transactions: Transaction[] = [];
+  for (let i = 1; i <= 50; i++) {
+    transactions.push({
+      id: i,
+      name: random.name({ nationality: 'en' }),
+      date: random.date(),
+      amount: random.dollar({ max: 10000 }),
+      donation: random.bool({ likelihood: 70 })
+    });
+  };
+
+  return transactions;
+}
 
 // module.exports = router;
 export default router;
---
+++ server/types/Transaction.ts
@@ -0,0 +1,7 @@
+export type Transaction = {
+  id: number;
+  name: string;
+  amount: string;
+  donation: boolean;
+  date: Date;
+}
