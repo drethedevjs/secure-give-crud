3057c99af9fd931df22ffe9d0dee5eed62954d36
Andre Thomas <drethedevjs@gmail.com>
2023-05-06 00:34:54 -0400

    Added edit form to the modal as a separate component.

---
+++ church-transactions/src/TransactionEditForm.tsx
@@ -0,0 +1,28 @@
+import { ChangeEvent } from "react";
+import { Form } from "react-bootstrap";
+import { FormDataProps } from "./PropInterfaces.js";
+
+export default function TransactionEditForm({ transactionFormData, setTransactionFormData } : FormDataProps) {
+
+  const handleNameChange = (e: ChangeEvent) => setTransactionFormData({ ...transactionFormData, name: (e.target as HTMLInputElement).value })
+  const handleAmountChange = (e: ChangeEvent) => setTransactionFormData({ ...transactionFormData, amount: (e.target as HTMLInputElement).value })
+  const handleDonationToggle = (e: ChangeEvent) => setTransactionFormData({ ...transactionFormData, donation: (e.target as HTMLInputElement).checked })
+
+  return (
+    <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" value={transactionFormData.name} onChange={handleNameChange} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Amount</Form.Label>
+        <Form.Control type="text" value={transactionFormData.amount.replace("$", "")} onChange={handleAmountChange} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Check type="checkbox" label="Donation" checked={transactionFormData.donation} onChange={handleDonationToggle} />
+      </Form.Group>
+    </Form>
+  )
+}
